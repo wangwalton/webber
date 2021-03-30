6593b4e38a9e2c2b665f6b77d72ddc01508bd68b
Walton Wang <yuwaltonwang@gmail.com>
2021-03-29 23:29:58 -0400

    backend

---
+++ web_backend/index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import { readFileSync } from "fs";
+
+// TODO
+import MONGODB from "mongodb";
+const { MongoClient, ObjectID } = MONGODB;
+
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const MONGO_URL = process.env.MONGO_URL;
+
+const mongoClient = await MongoClient.connect(MONGO_URL, {});
+const db = mongoClient.db("webber");
+const jobs = db.collection("jobs");
+
+const typeDefs = readFileSync("./schema.graphql").toString("utf-8");
+
+const resolvers = {
+  Query: {
+    getMyJobs: async () => {
+      console.log("getMyjobs:");
+      const res = await jobs.find().toArray();
+      console.log(res);
+      return res;
+    },
+  },
+  Mutation: {
+    createJob: async (_, { job }) => {
+      console.log(job);
+      delete job._id;
+      job.userID = new ObjectID();
+      try {
+        const res = await jobs.insertOne(job);
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
+    updateJob: async (_, { job }) => {
+      job.userID = new ObjectID();
+
+      const _id = new ObjectID(job._id);
+      delete job._id;
+      try {
+        const res = await jobs.updateOne({ _id }, { $set: job });
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+app.use(cors());
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
